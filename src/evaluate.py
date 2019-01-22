d79fd075f78f53504b9f17feb979edf86c5bf15e
Calvin Wang <calvin.wang0628@gmail.com>
2019-01-22 02:41:14 -0800

    Updated evaluation to to run through all models and only graph ones with profit

--- src/evaluate.py
+++ src/evaluate.py
@@ -8,54 +8,60 @@ from agent import Agent
 from functions import *
 import sys
 import os
-import matplotlib.pyplot as plt
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
-if len(sys.argv) != 3:
-    print("Usage: python evaluate.py [stock] [model]")
+if len(sys.argv) != 2:
+    # print("Usage: python evaluate.py [stock] [model]")
+    print("Usage: python evaluate.py [stock]")
     exit()
 
-stock_name, model_name = sys.argv[1], sys.argv[2]
-model = load_model("../models/SR_models/" + model_name)
-window_size = model.layers[0].input.shape.as_list()[1]
+for eNum in range(10, 1001, 10):
+    # stock_name, model_name = sys.argv[1], sys.argv[2]
+    stock_name = sys.argv[1]
+    model_name = "model_ep" + eNum
+    model = load_model("../models/SR_models/" + model_name)
+    window_size = model.layers[0].input.shape.as_list()[1]
 
-agent = Agent(window_size, True, model_name)
-data = getStockDataVec(stock_name)
-l = len(data) - 1
-batch_size = 32
+    agent = Agent(window_size, True, model_name)
+    data = getStockDataVec(stock_name)
+    l = len(data) - 1
+    batch_size = 32
 
-state = getState(data, 0, window_size + 1)
-total_profit = 0
-agent.inventory = []
-buy = [0] * len(data)
-sell = [0] * len(data)
+    state = getState(data, 0, window_size + 1)
+    total_profit = 0
+    agent.inventory = []
+    buy = [0] * len(data)
+    sell = [0] * len(data)
 
-for t in range(l):
-    action = agent.act(state)
+    for t in range(l):
+        action = agent.act(state)
 
-    # sit
-    next_state = getState(data, t + 1, window_size + 1)
-    reward = 0
+        # sit
+        next_state = getState(data, t + 1, window_size + 1)
+        reward = 0
 
-    if action == 1:  # buy
-        agent.inventory.append(data[t])
-        print("Buy: " + formatPrice(data[t]))
-        buy[t] = formatPrice(data[t])
+        if action == 1:  # buy
+            agent.inventory.append(data[t])
+            print("Buy: " + formatPrice(data[t]))
+            buy[t] = formatPrice(data[t])
 
-    elif action == 2 and len(agent.inventory) > 0:  # sell
-        bought_price = agent.inventory.pop(0)
-        reward = max(data[t] - bought_price, 0)
-        total_profit += data[t] - bought_price
-        print("Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price))
-        sell[t] = formatPrice(data[t])
+        elif action == 2 and len(agent.inventory) > 0:  # sell
+            bought_price = agent.inventory.pop(0)
+            reward = max(data[t] - bought_price, 0)
+            total_profit += data[t] - bought_price
+            print("Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price))
+            sell[t] = formatPrice(data[t])
 
-    done = True if t == l - 1 else False
-    agent.memory.append((state, action, reward, next_state, done))
-    state = next_state
+        done = True if t == l - 1 else False
+        agent.memory.append((state, action, reward, next_state, done))
+        state = next_state
 
-    if done:
-        graph(data, buy, sell, model_name)
-        print("--------------------------------")
-        print(stock_name + " Total Profit: " + formatPrice(total_profit))
-        print("--------------------------------")
+        if done:
+            print("--------------------------------")
+            print(stock_name + " Total Profit: " + formatPrice(total_profit))
+            print("--------------------------------")
+
+            if total_profit > 0:
+                print("Winner winner chicken dinner: " + model_name)
+                graph(data, buy, sell, model_name)
