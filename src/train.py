d351cd53a0994ec4667f160292246cad015c1149
Calvin Wang <calvin.wang0628@gmail.com>
2019-08-27 16:47:07 -0700

    train method abstraction

--- src/train.py
+++ src/train.py
@@ -16,58 +16,59 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 stock_name, window_size, episode_count = sys.argv[1], int(sys.argv[2]), int(sys.argv[3])
 
-agent = Agent(window_size)
-data = getStockDataVec(stock_name)
-l = len(data) - 1
-batch_size = 32
-punishment = -500
+def train(stock_name, window_size, episode_count): 
+    agent = Agent(window_size)
+    data = getStockDataVec(stock_name)
+    l = len(data) - 1
+    batch_size = 32
+    punishment = -500
 
-for e in range(episode_count + 1):
-    print("Episode " + str(e) + "/" + str(episode_count))
-    state = getState(data, 0, window_size + 1)
+    for e in range(episode_count + 1):
+        print("Episode " + str(e) + "/" + str(episode_count))
+        state = getState(data, 0, window_size + 1)
 
-    total_profit = 0
-    agent.inventory = []
-    history = []
+        total_profit = 0
+        agent.inventory = []
+        history = []
 
-    for t in range(l):
-        action = agent.act(state)
-        history.append(action)
+        for t in range(l):
+            action = agent.act(state)
+            history.append(action)
 
-        # sit
-        next_state = getState(data, t + 1, window_size + 1)
-        reward = 0
+            # sit
+            next_state = getState(data, t + 1, window_size + 1)
+            reward = 0
 
-        if action == 0 and len(history) >= 50 and history[-50:] == [0] * 20:
-            print("PUNISHED: 50 consecutive snoozes")
-            reward = punishment
-
-        elif action == 1:  # buy
-            if len(history) >= 20 and history[-20:] == [1]*20:
+            if action == 0 and len(history) >= 50 and history[-50:] == [0] * 20:
+                print("PUNISHED: 50 consecutive snoozes")
                 reward = punishment
-                print("PUNISHED: 20 consecutive buys")
-                
-            else:
-                agent.inventory.append(data[t])
-                print("Buy: " + formatPrice(data[t]))
 
-        elif action == 2 and len(agent.inventory) > 0:  # sell
-            bought_price = agent.inventory.pop(0)
-            reward = (data[t] - bought_price) * 100
-            total_profit += data[t] - bought_price
-            print("Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price))
+            elif action == 1:  # buy
+                if len(history) >= 20 and history[-20:] == [1]*20:
+                    reward = punishment
+                    print("PUNISHED: 20 consecutive buys")
+                    
+                else:
+                    agent.inventory.append(data[t])
+                    print("Buy: " + formatPrice(data[t]))
 
-        done = True if t == l - 1 else False
-        agent.memory.append((state, action, reward, next_state, done))
-        state = next_state
+            elif action == 2 and len(agent.inventory) > 0:  # sell
+                bought_price = agent.inventory.pop(0)
+                reward = (data[t] - bought_price) * 100
+                total_profit += data[t] - bought_price
+                print("Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price))
 
-        if done:
-            print("--------------------------------")
-            print("Total Profit: " + formatPrice(total_profit))
-            print("--------------------------------")
+            done = True if t == l - 1 else False
+            agent.memory.append((state, action, reward, next_state, done))
+            state = next_state
 
-        if len(agent.memory) > batch_size:
-            agent.expReplay(batch_size)
+            if done:
+                print("--------------------------------")
+                print("Total Profit: " + formatPrice(total_profit))
+                print("--------------------------------")
 
-    if e % 10 == 0:
-        agent.model.save("../models/SR_models/model_ep" + str(e))
+            if len(agent.memory) > batch_size:
+                agent.expReplay(batch_size)
+
+        if e % 10 == 0:
+            agent.model.save("../models/SR_models/model_ep" + str(e))
